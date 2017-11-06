9f3abc0a7ae5b4b9b704c7661eb4402d5dbc7451
John Conway <jpatrick9793@gmail.com>
2017-11-05 22:53:29 -0500

    Added OAuth 2.0 login and log out

--- models.py
+++ models.py
@@ -1,29 +1,65 @@
-from sqlalchemy import Column,Integer,String
+# DB FILE FOR CATALOG PROJECT
+
+from sqlalchemy import Column,Integer,String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship, sessionmaker
 from sqlalchemy import create_engine
 from passlib.apps import custom_app_context as pwd_context
+import random, string
+from itsdangerous import(TimedJSONWebSignatureSerializer as Serializer, BadSignature, SignatureExpired)
 
 Base = declarative_base()
 
-class Items(Base):
-    __tablename__ = 'items'
+secret_key = ''.join(random.choice(string.ascii_uppercase + string.digits) for x in xrange(32))
+
+class User(Base):
+    __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
-    name = Column(String)
-    # picture = Column(String)
-    description = Column(String)
-    # price = Column(String)
+    username = Column(String(32), index=True)
+    picture = Column(String)
+    email = Column(String)
+    # children = relationship("Items")
     @property
     def serialize(self):
         """Return object data in easily serializeable format"""
         return {
-        'name' : self.name,
-        # 'picture' : self.picture,
-        # 'price' : self.price,
+        'id'           : self.id,
+        'username'     : self.username,
+        'email'        : self.email,
+        'password_hash': self.password_hash
+            }
+
+    @staticmethod
+    def verify_auth_token(token):
+    	s = Serializer(secret_key)
+    	try:
+    		data = s.loads(token)
+    	except SignatureExpired:
+    		#Valid Token, but expired
+    		return None
+    	except BadSignature:
+    		#Invalid Token
+    		return None
+    	user_id = data['id']
+    	return user_id
+
+class Items(Base):
+    __tablename__ = 'items'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id') )
+    name = Column(String)
+    description = Column(String)
+
+    @property
+    def serialize(self):
+        """Return object data in easily serializeable format"""
+        return {
+        'id'          : self.id,
+        'user_id'     : self.user_id,
+        'name'        : self.name,
         'description' : self.description
             }
 
 engine = create_engine('sqlite:///catalog.db')
- 
 
 Base.metadata.create_all(engine)
--- server.py
+++ server.py
@@ -1,63 +1,209 @@
-from redis import Redis
-import time
-from functools import update_wrapper
-from flask import request, g
-from flask import Flask, jsonify 
-from models import Base, Items
-
+from flask import request, g, Flask, jsonify, make_response, render_template
+from flask import redirect, url_for, flash
+from flask.ext.httpauth import HTTPBasicAuth
+from flask import session as login_session
 
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship, sessionmaker
 from sqlalchemy import create_engine
 
+from oauth2client.client import flow_from_clientsecrets
+from oauth2client.client import FlowExchangeError
+
+from models import Base, Items, User
+
+import httplib2
+import requests
 import json
+import random, string
 
 engine = create_engine('sqlite:///catalog.db')
-
-
 Base.metadata.bind = engine
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 app = Flask(__name__)
+auth = HTTPBasicAuth()
 
-
-app = Flask(__name__)
-redis = Redis()
-
+CLIENT_ID = json.loads(
+    open('client_secrets.json', 'r').read())['web']['client_id']
 
 @app.route('/')
 @app.route('/home')
 def getHomePage():
-	return "This will be the HomePage"
+    users = session.query(User).all()
+    return render_template('homepage.html', users = users, login_session = login_session)
 
 
 @app.route('/login')
 def getLogin():
-	return "This will be the login page"
+  state = ''.join( random.choice( string.ascii_uppercase + string.digits ) for x in xrange(32))
+  login_session['state'] = state
+  return render_template('login.html', STATE = state)
 
-@app.route('/logout')
-def getLogout():
-	return "This will be the logout page"
+
+@app.route('/gconnect', methods=['POST'])
+def gconnect():
+    # Validate state token
+    if request.args.get('state') != login_session['state']:
+        response = make_response(json.dumps('Invalid state parameter.'), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+    # Obtain authorization code
+    code = request.data
+
+    try:
+        # Upgrade the authorization code into a credentials object
+        oauth_flow = flow_from_clientsecrets('client_secrets.json', scope='')
+        oauth_flow.redirect_uri = 'postmessage'
+        credentials = oauth_flow.step2_exchange(code)
+    except FlowExchangeError:
+        response = make_response(json.dumps('Failed to upgrade the authorization code.'), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    # Check that the access token is valid.
+    access_token = credentials.access_token
+    url = ('https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=%s'
+           % access_token)
+    h = httplib2.Http()
+    result = json.loads(h.request(url, 'GET')[1])
+    # If there was an error in the access token info, abort.
+    if result.get('error') is not None:
+        response = make_response(json.dumps(result.get('error')), 500)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    # Verify that the access token is used for the intended user.
+    gplus_id = credentials.id_token['sub']
+    if result['user_id'] != gplus_id:
+        response = make_response(
+            json.dumps("Token's user ID doesn't match given user ID."), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    # Verify that the access token is valid for this app.
+    if result['issued_to'] != CLIENT_ID:
+        response = make_response(
+            json.dumps("Token's client ID does not match app's."), 401)
+        print "Token's client ID does not match app's."
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    stored_access_token = login_session.get('access_token')
+    stored_gplus_id = login_session.get('gplus_id')
+    if stored_access_token is not None and gplus_id == stored_gplus_id:
+        response = make_response(json.dumps('Current user is already connected.'),
+                                 200)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    # Store the access token in the session for later use.
+    login_session['access_token'] = credentials.access_token
+    login_session['gplus_id'] = gplus_id
+
+    # Get user info
+    userinfo_url = "https://www.googleapis.com/oauth2/v1/userinfo"
+    params = {'access_token': credentials.access_token, 'alt': 'json'}
+    answer = requests.get(userinfo_url, params=params)
+
+    data = answer.json()
+
+    login_session['username'] = data['name']
+    login_session['picture'] = data['picture']
+    login_session['email'] = data['email']
+
+    user_id = getUserID(login_session['email'])
+    if not user_id:
+      user_id = createUser(login_session)
+    login_session['user_id'] = user_id
+
+    output = ''
+    output += '<h1>Welcome, '
+    output += login_session['username']
+    output += '!</h1>'
+    output += '<img src="'
+    output += login_session['picture']
+    output += ' " style = "width: 300px; height: 300px;border-radius: 150px;-webkit-border-radius: 150px;-moz-border-radius: 150px;"> '
+    flash("you are now logged in as %s" % login_session['username'])
+    print "done!"
+    return output
+
+
+@app.route('/gdisconnect')
+def gdisconnect():
+    access_token = login_session.get('access_token')
+    if access_token is None:
+        print 'Access Token is None'
+        response = make_response(json.dumps('Current user not connected.'), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    url = 'https://accounts.google.com/o/oauth2/revoke?token=%s' % login_session['access_token']
+    h = httplib2.Http()
+    result = h.request(url, 'GET')[0]
+
+    if result['status'] == '200':
+        del login_session['access_token']
+        del login_session['gplus_id']
+        del login_session['username']
+        del login_session['email']
+        del login_session['picture']
+        return redirect(url_for('getHomePage'), code=200)
+
+    else:
+        response = make_response(json.dumps('Failed to revoke token for given user.', 400))
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+
+@app.route('/delete/<int:id>', methods=['POST', 'GET'])
+def deleteUser(id):
+    deletedUser = session.query(User).filter_by(id = id).one()
+    if request.method == 'GET':
+        return jsonify(user = deletedUser.serialize)
+    if request.method == 'POST':
+        session.delete(deletedUser)
+        session.commit()
+        return "User has been removed"
 
 @app.route('/catalog/<int:user_id>')
 def getCatalog(user_id):
-	return ("List of all products for user %s" % user_id)
+    return ("List of all products for user %s" % user_id)
 
 @app.route('/catalog/<int:user_id>/create')
 def getCreate(user_id):
-	return ("Create a new entry into the database for user %s" % user_id)
+    return ("Create a new entry into the database for user %s" % user_id)
 
 @app.route('/catalog/<int:user_id>/update/<int:item_id>')
 def getUpdate(user_id, item_id):
-	return ("edit a current entry for item %s in the database for user %s" % (item_id, user_id) )
+    return ("edit a current entry for item %s in the database for user %s" % (item_id, user_id) )
 
 @app.route('/catalog/<int:user_id>/delete/<int:item_id>')
 def getDelete(user_id, item_id):
-	return ("delete item %s in the database for user %s" % (item_id, user_id) )
+    return ("delete item %s in the database for user %s" % (item_id, user_id) )
 
+
+def getUserID(email):
+  try:
+    user = session.query(User).filter_by(email = email).one()
+    return user.id
+  except:
+    return None
+
+def createUser( login_session ):
+  newUser = User( username = login_session['username'], email = login_session['email'], 
+    picture = login_session['picture'])
+  session.add(newUser)
+  session.commit()
+  user = session.query(User).filter_by(email = login_session['email']).one()
+  return user.id
+
+def getUserInfo(user_id):
+  user = session.query(User).filter_by(id=user_id).one()
+  return user
 
 if __name__ == '__main__':
-    # app.secret_key = 'super_secret_key'
+    app.secret_key = 'super_secret_key'
     app.debug = True
     app.run(host = '0.0.0.0', port = 8000)
 
